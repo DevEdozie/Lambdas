96ec881685ac01755764347da41e092c9191c686
nicholaus michael <nicholausmichael51@gmail.com>
2022-07-14 21:22:07 +0100

    more lambda

--- src/HigherOrderFunctions.kt
+++ src/HigherOrderFunctions.kt
@@ -11,16 +11,8 @@ fun setManual(name:String):String{
 }
 
 
-fun convert(x:Double, converter: (Double) -> Double):Double{
-    //invoke the lambda named converter
-    val result = converter(x)
-    println("$x is converted to $result")
-    return result
-}
-
 fun main(args: Array<String>) {
-    var converter = {c:Double -> c * 1.8 + 32}
-    var fahreheit = convert(20.0,converter)
+    var fahreheit = convert(20.0) {c:Double -> c * 1.8 + 32}
     var jumoke = printUsername("Jumoke",{name:String -> name})
     var forShola = {name:String ->
         "My name is $name"
@@ -28,4 +20,5 @@ fun main(args: Array<String>) {
     val result = forShola("Shola")
     var shola = setManual(result)
     println(shola)
+    var five = convertFive { it * 1.8 + 32 }
 }
--- src/Lambdas.kt
+++ src/Lambdas.kt
@@ -1,18 +1,22 @@
+
+
+fun convertFive(converter: (Int) -> Double):Double{
+    val result = converter(5)
+    println("5 is converted to $result")
+    return result
+}
+
+
+fun convert(x:Double, converter: (Double) -> Double):Double{
+    //invoke the lambda named converter and assign it's return value to result
+    val result = converter(x)
+    //print the result
+    println("$x is converted to $result")
+    return result
+}
+
 fun main(args: Array<String>) {
-    var addFive = {x:Int -> x + 5}
-    println("Pass 6 to addFive: ${addFive(6)}")
-    println("Pass 7 to addFive: ${addFive.invoke(7)}")
-
-    val addInts = {x:Int,y:Int -> x + y}
-    val result = addInts.invoke(6,7)
-
-    val intLambda:(Int,Int) -> Int = {x,y -> x * y}
-    println("Pass 10,11 ti intLambda: ${intLambda(10,11)}")
-
-    val addSeven: (Int) -> Int = {it + 7}
-    println("Pass 12 to addSeven: ${addSeven(12)}")
-
-    val myLambda: () -> Unit = {println("Hi!")}
-    myLambda()
+   convert(20.0) {it * 1.8 + 32}
+    convertFive { it * 1.8 + 32 }
 
 }
